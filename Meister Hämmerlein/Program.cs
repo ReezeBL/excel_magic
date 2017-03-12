5c5f359cc22d60edaa9fbb30b1e627af8247b923
ReezeBL <shilk.e82@gmail.com>
2017-03-12 14:53:35 +0300

    refuse from interop services

--- Meister Hämmerlein/Program.cs
+++ Meister Hämmerlein/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using Meister_Hämmerlein.WindowForms;
 
@@ -6,9 +7,45 @@ namespace Meister_Hämmerlein
 {
     internal class Program
     {
-        [STAThread]
-        private static void Main()
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public static void ShowConsoleWindow()
         {
+            var handle = GetConsoleWindow();
+
+            if (handle == IntPtr.Zero)
+            {
+                AllocConsole();
+            }
+            else
+            {
+                ShowWindow(handle, SW_SHOW);
+            }
+        }
+
+        public static void HideConsoleWindow()
+        {
+            var handle = GetConsoleWindow();
+
+            ShowWindow(handle, SW_HIDE);
+        }
+
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 5;
+
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            if(args.Length > 0)
+                ShowConsoleWindow();
             Application.Run(new MainWindow());
         }
     }
